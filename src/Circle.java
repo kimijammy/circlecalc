ee4dfed44f86719e0cac0203a44fd0a54166d5c1
kimijammy <125917969+kimijammy@users.noreply.github.com>
2023-03-21 21:38:57 +0000

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class Circle {
+public class Circle {
+    private String color;
+    private double radius;
+    private double area;
+
+    public Circle(String color, double radius){
+        setColor(color);
+        setRadius(radius);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public double getArea() {
+        return radius*radius*3.14;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
 }
