e6b352777a627c36a8b29531bec29dae6a4765f7
kimijammy <125917969+kimijammy@users.noreply.github.com>
2023-03-21 21:39:50 +0000

    Update circleTest.java

--- src/circleTest.java
+++ src/circleTest.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class circleTest {
+public class circleTest {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle("Red",12.5);
+        System.out.println("The color of the circle is "+circle1.getColor());
+        System.out.println("The radius of the circle is "+circle1.getRadius());
+        System.out.println("The area of the circle is "+circle1.getArea());
+    }
+
 }
